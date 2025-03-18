6f4176f9938129999493ac8a210e1961f7ca709f
Hossein <Hossein.kelisa@gmail.com>
2025-03-18 14:11:55 +0100

    resolving the issues

--- assignments/hackyourtemperature/app.js
+++ assignments/hackyourtemperature/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('hello from backend to frontend!');
+    res.send('Hello from backend to frontend!');
 });
 
 app.post('/weather', async (req, res) => {
@@ -15,27 +15,26 @@ app.post('/weather', async (req, res) => {
         const { cityName } = req.body;
 
         if (!cityName) {
-            return res
-                .status(400)
-                .json({ weatherText: 'City name is required!' });
-              }
-      
-              const response = await fetch(
-                  `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${keys.API_KEY}`
-              );
-      
-              if (!response.ok) {
-                  return res.status(404).json({ weatherText: 'City is not found!' });
-              }
-      
-              const data = await response.json();
-              const temperatureInCelsius = Math.floor(data.main.temp - 273.15);
-              res.status(200).json({
-                weatherText: `${cityName}: ${temperatureInCelsius}°C`
-            });
-        } catch (error) {
-            res.status(500).json({ error: 'Internal server error' });
+            return res.status(400).json({ weatherText: 'City name is required!' });
         }
-    });
-    
-    export { app };
+
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${keys.API_KEY}`
+        );
+
+        if (!response.ok) {
+            return res.status(404).json({ weatherText: 'City is not found!' });
+        }
+
+        const data = await response.json();
+        const temperatureInCelsius = Math.floor(data.main.temp - 273.15);
+
+        res.status(200).json({
+            weatherText: `${cityName}: ${temperatureInCelsius}°C`
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+export { app };
